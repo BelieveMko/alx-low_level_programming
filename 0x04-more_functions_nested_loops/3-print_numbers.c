feed898a4785906117886ee33bd66cd1696efab6
Believe Mukomberanwa <believemuko1@gmail.com>
2023-09-15 01:37:24 +0200

    print nums from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,17 @@
 /**
- * print_numbers - print 0-9
- * return: void
+ * print_numbers - 0-9
+ * return: -  void
  */
 
 void print_numbers(void)
 {
 	int c = 0;
+	char x;
 
 	while (c < 9)
 	{
-	
-		_putchar(c);
+		x = c + '0';
+		_putchar(x);
 		c++;
 	}
 _putchar(10);
