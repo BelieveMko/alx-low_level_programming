4c80cecb2642fc2b341718d243d800c860722790
Believe Mukomberanwa <believemuko1@gmail.com>
2023-09-25 15:08:56 +0200

    prints in reverse follwed by a new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to be reversed
+ * return: - 0
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+
+	while (i > 0)
+	{
+		putchar(*s);
+		str--;
+	}
+
+	_putchar('\n');
+}
+
