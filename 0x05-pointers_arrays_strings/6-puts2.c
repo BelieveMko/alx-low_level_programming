fe8ebe9ecb7d6b9618b95a571c6ec5537e370d1b
Believe Mukomberanwa <believemuko1@gmail.com>
2023-09-26 12:13:37 +0200

    skips chars

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,30 @@
 #include "main.h"
 /**
- * puts2 - function should print only one character after another
+ * puts2 - function should print only one character out of two
  * starting with the first one
- * @str: input to the function
- * Return: print
+ * @str: variable
+ * Return: printout the chars
  */
 void puts2(char *str)
 {
-	while(*str != '\0')
-	{
-	_putchar(*str);
-	
-	str++;
-	str++;
-	}
+	int i = 0;
+	int m = 0;
+	char *y = str;
+	int x;
 
-	_putchar('\n');
+	while (*y != '\0')
+	{
+		y++;
+		i++;
+	}
+	m = i - 1;
+	for (x = 0 ; x <= m ; x++)
+	{
+		if (x % 2 == 0)
+	{
+		_putchar(str[x]);
+	}
+	}
+	_putchar(10);
 }
 
