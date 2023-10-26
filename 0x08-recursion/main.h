02b6edf4f3ddb7746a69763074636e6456efec01
Believe Mukomberanwa <believemuko1@gmail.com>
2023-10-27 00:45:56 +0200

    prime number calclator

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,8 @@
 int _putchar(char c);
 void _puts_recursion(char *s);
+int factorial(int n);
 void _print_rev_recursion(char *s);
 int calsquare(int a, int i);
 int _sqrt_recursion(int n);
-
+int primenumbercalculator(int n, int a);
 
