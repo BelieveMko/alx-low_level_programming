de5cd048589d7433628d1c3d75b00f3d1d595c6c
Believe Mukomberanwa <believemuko1@gmail.com>
2023-10-27 00:16:04 +0200

    factorial calculator

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+  * factorial :- function to calculate factorial
+  * @n: number to calculate factorial
+  *
+  * Return:- factorial
+  */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	else
+	{
+
+	}	
+	if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+
+	}
+
+return (n * factorial(n - 1));
+}
+
