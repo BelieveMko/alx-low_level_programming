1ba170e0723316c0021dcacbe4f42975a53f7556
Believe Mukomberanwa <believemuko1@gmail.com>
2023-10-27 00:10:27 +0200

    reverse string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,33 +11,8 @@ void _print_rev_recursion(char *s)
 
 {
 	if (*s != '\0')
-
 	{
-
-		if (*(s + 1) == '\0')
-		{
-
-			return;
-		}
-
-		else
-
-		{
-
-		s++;
-		_print_rev_recursion(s);
-
-		}
-
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 	}
-
-	else
-
-	{
-
-	}
-
-_putchar(*s);
-
-return;
 }
