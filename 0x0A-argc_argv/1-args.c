e73db4786ff45e2a6154684add8cd5651f5e1145
Believe Mukomberanwa <believemuko1@gmail.com>
2023-10-27 16:39:40 +0200

    prints the number of arguements

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * main - print the number of arguments
+  * @argc: number of arguments
+  * @argv: array of arguments
+  *
+  * Return:- return 0
+  */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+
+{
+	int x = argc - 1;
+
+	printf("%d\n", x);
+
+return (0);
+}
+
