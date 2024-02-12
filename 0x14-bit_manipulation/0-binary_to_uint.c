a79877e0d4747c21275b191bf8177047ac9b3153
Believe Mukomberanwa <believemuko1@gmail.com>
2024-02-12 16:47:21 +0200

    binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,24 +8,38 @@
  */
 
 unsigned int binary_to_uint(const char *b)
+
 {
-	unsigned int base = 1, res = 0, len = 0;
+	unsigned int n = 0;
+	unsigned int a = 0;
+	unsigned int c = 0;
+	unsigned int res = 0;
+	unsigned int bb;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[len])
-		len++;
-
-	while (len)
+	while (b[a] != '\0')
 	{
-		if (b[len - 1] != '0' && b[len - 1] != '1')
+		if (b[a] == '0' || b[a] == '1')
+		{
+			a++;
+		}
+		else
+		{
 			return (0);
+		}
+	}
 
-		if (b[len - 1] == '1')
-			res += base;
-		base *= 2;
-		len--;
+	c = a - 1;
+
+	while (a)
+	{
+		bb = b[n] - '0';
+		res = res + (bb * (1 << c));
+		n++;
+		a--;
+		c--;
 	}
 	return (res);
 }
