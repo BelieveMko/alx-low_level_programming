9bf741fa7174fbfdf5dd4b43b50e4e5bb6c1d308
Believe Mukomberanwa <believemuko1@gmail.com>
2024-02-14 10:44:56 +0200

    reads a file and prints contents on standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,37 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
+ * read_textfile - reads a text file and prints the letters on standard output
+ * @filename: text file to read from
+ * @letters: maximum  numbers of letters to be read form the text file.
  *
- * Return: numbers of letters printed. It fails, returns 0.
+ * Return: numbers of letters printed, if it fails, returns 0.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+	ssize_t actual_bytes_read, actual_bytes_written;
+	char *buffer;
 
 	if (!filename)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDWR);
 
 	if (fd == -1)
 		return (0);
 
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
+	buffer = malloc(sizeof(char) * (letters));
+	if (buffer == NULL)
 		return (0);
 
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
+	actual_bytes_read = read(fd, buffer, letters);
+	actual_bytes_written = write(STDOUT_FILENO, buffer, actual_bytes_read);
 
 	close(fd);
 
-	free(buf);
+	free(buffer);
 
-	return (nwr);
+	return (actual_bytes_written);
 }
