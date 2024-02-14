1ca45d2667720295dd98cf798e28a66aec231d3b
Believe Mukomberanwa <believemuko1@gmail.com>
2024-02-14 11:45:59 +0200

    create a file and write text in it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,13 +10,13 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int nletters;
-	int rwr;
+	int textlen = 0;
+	int error;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 
 	if (fd == -1)
 		return (-1);
@@ -24,12 +24,14 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
+	while (text_content[textlen])
+	{
+		textlen++;
+	}
 
-	rwr = write(fd, text_content, nletters);
+	error = write(fd, text_content, textlen);
 
-	if (rwr == -1)
+	if (error == -1)
 		return (-1);
 
 	close(fd);
