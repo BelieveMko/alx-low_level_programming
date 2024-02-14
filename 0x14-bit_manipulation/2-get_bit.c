543e413bc5600efbff75a5dc8568e41e45396d7c
Believe Mukomberanwa <believemuko1@gmail.com>
2024-02-14 13:02:28 +0200

    corrected variable spelling error

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,7 +9,7 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int coverted = 1;
+	unsigned long int converted = 1;
 
 	converted = converted << index;
 	if (converted == 0)
